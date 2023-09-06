3f0c70502ea58947a9fe24a47d010b4ab56c21f4
unknown <lab@204-11-knd.sliitstd.local>
2023-09-06 10:30:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tute4nextactiuvity/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.tute4nextactiuvity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var edtname: EditText
+    lateinit var btnwelcome:Button
+    lateinit var btngoodbye:Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtname = findViewById(R.id.edtname)
+        btnwelcome = findViewById(R.id.edtname)
+        btngoodbye = findViewById(R.id.btngoodbye)
+
+        fun sayHello(){
+            Toast.makeText(this, "Hello, ${edtname.text.toString()}",Toast.LENGTH_LONG).show()
+        }
+        fun sayGoodBye(){
+            Toast.makeText(this, "Hello, ${edtname.text.toString()}",Toast.LENGTH_LONG).show()
+        }
+
+        btnwelcome.setOnClickListener {
+            sayHello()
+        }
+
+        btngoodbye.setOnClickListener {
+            sayGoodBye()
+        }
+
+    }
+}
